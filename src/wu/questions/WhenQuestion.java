7d48301d4387ad3a27772da11a88e65628deea73
Joris Deguet <joris.deguet@gmail.com>
2010-10-11 11:26:09 -0400

    Changed the Date picking question in the Add One dialog to facilitate the input of ancient dates, note: there is a strange bug in the GWT datepicker component for months before year 1900, when we click on the arrow to change the month, the year changes also but it should not

--- src/wu/questions/WhenQuestion.java
+++ src/wu/questions/WhenQuestion.java
@@ -2,29 +2,65 @@ package wu.questions;
 
 import java.util.Date;
 
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.i18n.client.DateTimeFormat;
+import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.user.datepicker.client.DateBox;
 import com.google.gwt.user.datepicker.client.DatePicker;
 
 public class WhenQuestion extends Question<String>{
 
+	Date current;
+	
 	public WhenQuestion(String in) {
 		super(in);
 	}
 
 	@Override
 	public Widget answerPan() {
-		HorizontalPanel panel = new HorizontalPanel();
-		DatePicker a = new DatePicker();
-		a.addValueChangeHandler(new ValueChangeHandler<Date>(){
+		FlexTable panel = new FlexTable();
+		final DateTimeFormat dtf = DateTimeFormat.getFormat("yyyy");
+		
+		current = new Date();
+		
+		Label start = new Label("Start");
+		panel.setWidget(0, 0, start);
+		
+		DateBox box = new DateBox();
+		box.addValueChangeHandler(new ValueChangeHandler<Date>(){
 			@Override
 			public void onValueChange(ValueChangeEvent<Date> event) {
-				answerChannel().shareEvent(event.getValue().toGMTString());
+				current = event.getValue();
+				share();
 			}});
-		panel.add(a);
+		box.setFormat(new DateBox.Format(){
+			DateTimeFormat dtf = DateTimeFormat.getFormat("yyyy-MM-dd");
+			@Override
+			public String format(DateBox dateBox, Date date) {
+				return dtf.format(date!=null?date:new Date());
+			}
+			@Override
+			public Date parse(DateBox dateBox, String text, boolean reportError) {
+				return dtf.parse(text);
+			}
+			@Override
+			public void reset(DateBox dateBox, boolean abandon) {}});
+		panel.setWidget(0, 1, box);
 		return panel;
 	}
+	
+	private void share(){
+		answerChannel().shareEvent(current.toGMTString());
+	}
+	
+	
 }
 
